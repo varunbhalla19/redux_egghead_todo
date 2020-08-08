aea059fdf74916c816f53ecf109a77752a6cadfc
varunbhalla19 <varunbhalla19@gmail.com>
2020-08-08 13:29:58 +0530

    Used Provider component and Connect function to generate Container Components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
 
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+
+import { connect } from 'react-redux';
 
 let someId = 100
 
@@ -103,12 +105,9 @@ const showFilteredTodos = (ar, fil) => {
   return ar
 }
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer)
 
-const StoreContext = React.createContext(store);
-
-
-const Links = ({ active, clickHandler, children }) => (
+const Links = ({ active, children, clickHandler }) => (
   <button
     style={{ color: active ? 'indianRed' : 'black' }}
     onClick={clickHandler}
@@ -117,20 +116,37 @@ const Links = ({ active, clickHandler, children }) => (
   </button >
 )
 
-const FilterLinks = ({ filter }) => (
-  <StoreContext.Consumer>
-    {store => (
-      <Links
-        clickHandler={() => store.dispatch({ type: 'SET_FILTER', filter: filter })}
-        currentFilter={store.getState().filter}
-        active={filter === store.getState().filter}
-      >
-        {filter}
-      </Links>
-    )
-    }
-  </StoreContext.Consumer>
-)
+const mapStateToPropsLinks = (state, { filter }) => ({
+  currentFilter: state.filter,
+  active: (filter === state.filter),
+  children: filter
+})
+
+const mapDispatchToPropsLinks = (dispatch, { filter }) => ({
+  clickHandler: () => dispatch({
+    type: 'SET_FILTER',
+    filter: filter
+  })
+})
+
+const FilterLinks = connect(
+  mapStateToPropsLinks, mapDispatchToPropsLinks
+)(Links)
+
+// const FilterLinks = ({ filter }) => (
+//   <StoreContext.Consumer>
+//     {store => (
+//       <Links
+//         clickHandler={() => store.dispatch({ type: 'SET_FILTER', filter: filter })}
+//         currentFilter={store.getState().filter}
+//         active={filter === store.getState().filter}
+//       >
+//         {filter}
+//       </Links>
+//     )
+//     }
+//   </StoreContext.Consumer>
+// )
 
 const FilterLinkList = () => (
   <div>
@@ -140,64 +156,81 @@ const FilterLinkList = () => (
   </div>
 );
 
+
+
 const TodoItem = ({ clickHandle, completed, text, id }) => (
   <h5
-    onClick={ev => clickHandle(id)}
+    onClick={() => clickHandle(id)}
+
     style={{ color: completed ? 'red' : 'whitesmoke' }}
-  > {text} </h5>
+  > {text}  </h5>
 )
 
-
+// {console.log(todoArray , clickHandle )}
 const TodoList = ({ todoArray, clickHandle }) => (
   <div>
     {todoArray.map(tod => <TodoItem clickHandle={clickHandle} key={tod.id} {...tod} />)}
   </div>
 )
 
-const TheTodo = () => (
-  <StoreContext.Consumer>
-    {store => (
-      <TodoList
-        todoArray={showFilteredTodos(store.getState().todos, store.getState().filter)}
-        clickHandle={id => store.dispatch({ type: 'TOGGLE', id: id })}
-      />
-    )}
-  </StoreContext.Consumer>
-)
+const mapStateToPropsTodo = state => ({
+  todoArray: showFilteredTodos(
+    state.todos, state.filter
+  )
+})
+
+const mapDispatchToPropsTodo = dispatch => ({
+  clickHandle: id => dispatch({ type: 'TOGGLE', id: id })
+})
+
+const TheTodo = connect(
+  mapStateToPropsTodo, mapDispatchToPropsTodo
+)(TodoList)
+
+// const TheTodo = () => (
+//   <StoreContext.Consumer>
+//     {store => (
+//       <TodoList
+//         todoArray={showFilteredTodos(store.getState().todos, store.getState().filter)}
+//         clickHandle={id => store.dispatch({ type: 'TOGGLE', id: id })}
+//       />
+//     )}
+//   </StoreContext.Consumer>
+// )
 
 
-const AddTodo = ({ store }) => {
+
+let AddTodo = ({ dispatch }) => {
   let input;
   return (
     <div>
+
       <input type="text" placeholder="Add Todos..."
         ref={node => (input = node)} />
 
       <button onClick={ev => {
-        input.value && store.dispatch({ type: 'ADD', text: input.value, id: someId++ });
+        input.value && dispatch({ type: 'ADD', text: input.value, id: someId++ });
         input.value = null;
       }}
       >Add Todo</button>
     </div>
   )
 }
+AddTodo = connect(
+  null, null
+)(AddTodo)
 
-const Header = ({ store }) => (
-  <StoreContext.Consumer>
-    {
-      store => (
-        <h2> Helloooo {store.getState().username} </h2>
-      )
-    }
-  </StoreContext.Consumer>
-)
 
-const Todo = ({ store }) => (
+let Header = ({ username }) => (
+  <h2> Helloooo {username} </h2>
+);
+Header = connect(state => (state))(Header)
+
+
+const Todo = ({ filter, dispatch }) => (
   <div className="App" >
 
-    <StoreContext.Provider value={store} >
-      <Header />
-    </StoreContext.Provider>
+    <Header />
 
     <AddTodo store={store} />
 
@@ -205,29 +238,25 @@ const Todo = ({ store }) => (
 
     <TheTodo />
 
-    <p> Filter : {store.getState().filter} </p>
-
-    <button
-      onClick={ev => store.dispatch({ type: 'SET_USER', username: 'Rabbit' })}
-    >
-      Change User </button>
+    <p> Filter : {filter} </p>
+    <button onClick={ev => dispatch({ type: 'SET_USER', username: 'Rabbit' })} >
+      Change User
+    </button>
 
   </div>
 )
 
+const RealTodo = connect(
+  state => ({ filter: state.filter }), null
+)(Todo)
 
-const renderr = () => {
-  ReactDOM.render(
-    <Todo store={createStore(rootReducer)} />
-    ,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <Provider store={store} >
+    <RealTodo />
+  </Provider>
+  ,
+  document.getElementById('root')
+);
 
-renderr();
-
-store.subscribe(() => {
-  console.log(store.getState());
-  renderr();
-})
+console.log(store.getState())
 
